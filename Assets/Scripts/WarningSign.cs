43e7b1909167292656de45c81609e17fe18837d4
krzysztofskorek <57440106+krzysztofskorek@users.noreply.github.com>
2026-01-28 13:37:39 +0100

    Initial commit.

--- Assets/Scripts/WarningSign.cs
+++ Assets/Scripts/WarningSign.cs
@@ -2,6 +2,12 @@ using UnityEngine;
 
 public class WarningSign : MonoBehaviour
 {
+
+    [Tooltip("Scroll speed in X direction")]
+    public float scrollSpeedX = 0.5f;
+
+    private Material materialInstance;
+    private Vector2 currentOffset;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
