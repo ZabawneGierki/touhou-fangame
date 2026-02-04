d2fb5c084fb268e615bd808625a27d74257fe2aa
krzysztofskorek <57440106+krzysztofskorek@users.noreply.github.com>
2026-02-04 15:55:37 +0100

    finished settings

--- Assets/Scripts/Player/Shooter/MasterSpark.cs
+++ Assets/Scripts/Player/Shooter/MasterSpark.cs
@@ -1,64 +1,53 @@
+using System.Collections;
 using UnityEngine;
 using static UnityEngine.RuleTile.TilingRuleOutput;
 
 [CreateAssetMenu(fileName = "MasterSpark", menuName = "Scriptable Objects/Shooters/MasterSpark")]
 public class MasterSpark : ShootData
 {
-    [Header("Master Spark")]
-    public LineRenderer line;
     public float maxDistance = 20f;
     public float damagePerSecond = 25f;
     public LayerMask hitMask;
+    public LineRenderer linePrefab;
+    public ParticleSystem hitParticlesPrefab;
 
+    private Coroutine shootingCoroutine;
 
-    [Header("Effects")]
-    public ParticleSystem hitParticles;
     public override void StartShooting(GameObject player)
     {
-        Debug.Log("Master Spark Activated");
+        if (shootingCoroutine == null)
+        {
+             
+            // Fix: Get MonoBehaviour from player to start coroutine
+            if (player.TryGetComponent<MonoBehaviour>(out var mono))
+            {
+                shootingCoroutine = mono.StartCoroutine(ShootSpark());
+            }
+            else
+            {
+                Debug.LogError("Player GameObject does not have a MonoBehaviour to start coroutine.");
+            }
+        }
+
     }
 
     public override void StopShooting(GameObject player)
     {
-       Debug.Log("Master Spark Deactivated");
+        if (shootingCoroutine != null)
+        {
+            if (player.TryGetComponent<MonoBehaviour>(out var mono))
+            {
+                mono.StopCoroutine(shootingCoroutine);
+            }
+            shootingCoroutine = null;
+        }
+
     }
 
-    void FireLaser()
+    private IEnumerator ShootSpark()
     {
-        line.enabled = true;
-
-        Vector2 origin = shootingPoint.position;
-        Vector2 direction = Vector2.up;
-
-        RaycastHit2D hit = Physics2D.Raycast(origin, direction, maxDistance, hitMask);
-
-        Vector3 endPos;
-
-        if (hit.collider != null)
-        {
-            endPos = hit.point;
-
-            // Damage
-            //if (hit.collider.TryGetComponent<IDamageable>(out var dmg))
-            //{
-            //    dmg.TakeDamage(damagePerSecond);
-            //}
-
-            // Particles
-            if (!hitParticles.isPlaying)
-                hitParticles.Play();
-
-            hitParticles.transform.position = hit.point;
-        }
-        else
-        {
-            endPos = origin + direction * maxDistance;
-
-            if (hitParticles.isPlaying)
-                hitParticles.Stop();
-        }
-
-        line.SetPosition(0, origin);
-        line.SetPosition(1, endPos);
+        int counter = 0;
+        yield return new WaitForSeconds(1f);
+        Debug.Log("Counter: " + counter);
     }
 }
