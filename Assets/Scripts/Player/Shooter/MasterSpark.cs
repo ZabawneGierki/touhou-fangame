4dc18eec738d914f6f065636a50214100baaa9a0
krzysztofskorek <57440106+krzysztofskorek@users.noreply.github.com>
2026-02-04 15:05:53 +0100

    finished settings

--- Assets/Scripts/Player/Shooter/MasterSpark.cs
+++ Assets/Scripts/Player/Shooter/MasterSpark.cs
@@ -3,7 +3,11 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "MasterSpark", menuName = "Scriptable Objects/Shooters/MasterSpark")]
 public class MasterSpark : ShootData
 {
-    
+    [Header("Master Spark")]
+    public LineRenderer line;
+    public float maxDistance = 20f;
+    public float damagePerSecond = 25f;
+    public LayerMask hitMask;
     public override void StartShooting(GameObject player)
     {
         Debug.Log("Master Spark Activated");
