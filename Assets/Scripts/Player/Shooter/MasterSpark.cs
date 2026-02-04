157866346cbf39151939d5c4625680e62c3fc665
krzysztofskorek <57440106+krzysztofskorek@users.noreply.github.com>
2026-02-04 15:09:46 +0100

    finished settings

--- Assets/Scripts/Player/Shooter/MasterSpark.cs
+++ Assets/Scripts/Player/Shooter/MasterSpark.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using static UnityEngine.RuleTile.TilingRuleOutput;
 
 [CreateAssetMenu(fileName = "MasterSpark", menuName = "Scriptable Objects/Shooters/MasterSpark")]
 public class MasterSpark : ShootData
@@ -8,6 +9,10 @@ public class MasterSpark : ShootData
     public float maxDistance = 20f;
     public float damagePerSecond = 25f;
     public LayerMask hitMask;
+
+
+    [Header("Effects")]
+    public ParticleSystem hitParticles;
     public override void StartShooting(GameObject player)
     {
         Debug.Log("Master Spark Activated");
@@ -16,5 +21,44 @@ public class MasterSpark : ShootData
     public override void StopShooting(GameObject player)
     {
        Debug.Log("Master Spark Deactivated");
+    }
+
+    void FireLaser()
+    {
+        line.enabled = true;
+
+        Vector2 origin = shootingPoint.position;
+        Vector2 direction = Vector2.up;
+
+        RaycastHit2D hit = Physics2D.Raycast(origin, direction, maxDistance, hitMask);
+
+        Vector3 endPos;
+
+        if (hit.collider != null)
+        {
+            endPos = hit.point;
+
+            // Damage
+            //if (hit.collider.TryGetComponent<IDamageable>(out var dmg))
+            //{
+            //    dmg.TakeDamage(damagePerSecond);
+            //}
+
+            // Particles
+            if (!hitParticles.isPlaying)
+                hitParticles.Play();
+
+            hitParticles.transform.position = hit.point;
+        }
+        else
+        {
+            endPos = origin + direction * maxDistance;
+
+            if (hitParticles.isPlaying)
+                hitParticles.Stop();
+        }
+
+        line.SetPosition(0, origin);
+        line.SetPosition(1, endPos);
     }
 }
