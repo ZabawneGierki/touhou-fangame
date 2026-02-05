f57434088127a99d2f9c0b70156577aeea5d8e16
krzysztofskorek <57440106+krzysztofskorek@users.noreply.github.com>
2026-02-05 16:18:54 +0100

    finished settings

--- Assets/Scripts/Pools/SparkInstance.cs
+++ Assets/Scripts/Pools/SparkInstance.cs
@@ -2,15 +2,57 @@ using UnityEngine;
 
 public class SparkInstance : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private LineRenderer lineRenderer;
+    private Transform originPoint;
+    private float damagePerSecond;
+    private float maxDistance;
+    private LayerMask enemyLayer;
+    private bool isActive;
+
+    void Awake()
     {
-        
+        lineRenderer = GetComponentInChildren<LineRenderer>();
     }
 
-    // Update is called once per frame
+    public void Setup(Transform origin, float damage, float dist, LayerMask mask)
+    {
+        originPoint = origin;
+        damagePerSecond = damage;
+        maxDistance = dist;
+        enemyLayer = mask;
+        isActive = true;
+        lineRenderer.enabled = true;
+    }
+
+    public void Deactivate()
+    {
+        isActive = false;
+        lineRenderer.enabled = false;
+    }
+
     void Update()
     {
-        
+        if (!isActive || originPoint == null) return;
+
+        Vector2 origin = originPoint.position;
+        Vector2 direction = originPoint.up; // Shoots "up" relative to the gun
+
+        RaycastHit2D hit = Physics2D.Raycast(origin, direction, maxDistance, enemyLayer);
+        lineRenderer.SetPosition(0, origin);
+
+        if (hit.collider != null)
+        {
+            lineRenderer.SetPosition(1, hit.point);
+
+            // Damage logic (Replace 'EnemyScript' with your actual script name)
+            if (hit.collider.TryGetComponent(out EnemyHealth enemy))
+            {
+                enemy.TakeDamage(damagePerSecond * Time.deltaTime);
+            }
+        }
+        else
+        {
+            lineRenderer.SetPosition(1, origin + direction * maxDistance);
+        }
     }
 }
