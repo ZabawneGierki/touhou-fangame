ff67c128d14316466d8264448b939e42cd2011fa
krzysztofskorek <57440106+krzysztofskorek@users.noreply.github.com>
2026-02-07 18:18:26 +0100

    finished settings

--- Assets/Scripts/Player/PowerUps/HakkeroHelpers.cs
+++ Assets/Scripts/Player/PowerUps/HakkeroHelpers.cs
@@ -1,7 +1,17 @@
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "HakkeroHelpers", menuName = "Scriptable Objects/HakkeroHelpers")]
-public class HakkeroHelpers : ScriptableObject
+[CreateAssetMenu(fileName = "HakkeroHelpers", menuName = "Scriptable Objects/PowerUps/HakkeroHelpers")]
+public class HakkeroHelpers : PowerUpData
 {
-    
+    [System.Serializable]
+    private struct HakkeroData
+    {
+        public string name;
+        public Hakkero helper;
+        public Vector2 positionOffset;
+    }
+    public override void PowerUp(Transform transform, int level)
+    {
+        throw new System.NotImplementedException();
+    }
 }
