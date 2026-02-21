4323449a23e4d4e14fe416af3b020694f2f36236
krzysztofskorek <57440106+krzysztofskorek@users.noreply.github.com>
2026-02-21 17:04:48 +0100

    removed some pix

--- Assets/Scripts/Player/PowerUps/Hakkero.cs
+++ Assets/Scripts/Player/PowerUps/Hakkero.cs
@@ -4,5 +4,9 @@ using UnityEngine;
 
 public class Hakkero : MonoBehaviour
 {
+    private void Update()
+    {
+        Debug.Log("Location of " + this.gameObject.name + ":" + transform.position);
+    }
 
 }
